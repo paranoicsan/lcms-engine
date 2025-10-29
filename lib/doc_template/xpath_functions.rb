fc268481f2ee585ca0804598e748fb3159991b38
Oksana Melnikova <oksana.melnikova@gmail.com>
2025-10-30 00:03:27 +0400

    updated xpath case_insensitive_equals

--- lib/doc_template/xpath_functions.rb
+++ lib/doc_template/xpath_functions.rb
@@ -3,7 +3,8 @@
 module DocTemplate
   class XpathFunctions
     def case_insensitive_equals(node_set, str_to_match)
-      return node_set.to_s.squish.casecmp(str_to_match).zero? if node_set.is_a?(String)
+      # remove spaces (will be added if there were sub elements) and compare case insensitively
+      return node_set.to_s.squish.gsub(/\s*/, '').casecmp(str_to_match).zero? if node_set.is_a?(String)
 
       node_set.find_all { |node| node.to_s.squish.casecmp(str_to_match).zero? }
     end
